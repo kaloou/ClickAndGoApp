48ab094cb63a016412a27148f47f5ebd6abf4eef
kalouu <lucaspalu866@gmail.com>
2026-02-11 17:45:47 +0100

    Initialize MVC base structure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ClickAndGoApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
