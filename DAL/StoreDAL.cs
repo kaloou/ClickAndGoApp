e55c39e6c6935eb4e763e5f48129d3aaca0ba3ee
byWaaa <190048400+byWaaa@users.noreply.github.com>
2026-05-09 15:39:22 +0200

    Added the storeDAL

--- DAL/StoreDAL.cs
+++ DAL/StoreDAL.cs
@@ -1,8 +1,40 @@
+using ClickAndGoApp.Models;
 using Microsoft.Data.SqlClient;
 namespace ClickAndGoApp.DAL;
 
 public class StoreDAL
 {
-    private readonly string db;
-    
+    private readonly DBConnection _db;
+
+    public StoreDAL(DBConnection db)
+    {
+        _db = db;
+    }
+
+    public Store GetStore(int storeId)
+    {
+        using SqlConnection conn = _db.GetConnexion();
+        conn.Open();
+        
+        string query = @"
+                SELECT storeId, name, address
+                FROM Store
+                WHERE storeId = @storeId";
+
+        using SqlCommand cmd = new SqlCommand(query, conn);
+        cmd.Parameters.AddWithValue("@storeId", storeId);
+
+        using SqlDataReader reader = cmd.ExecuteReader();
+
+        if (reader.Read())
+        {
+            return new Store(
+                (int)reader["storeId"],
+                (string)reader["name"],
+                (string)reader["address"]
+                );
+        }
+
+        return null;
+    }
 }
